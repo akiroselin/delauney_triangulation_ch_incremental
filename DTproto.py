cb97e1895b28e71fbf7c14f40630fa07f5683975
Aki Lin <amy881225@gmail.com>
2022-06-01 15:04:07 +0200

    Delet points one by one, find its neighbours

--- DTproto.py
+++ DTproto.py
@@ -1,10 +1,22 @@
 import numpy as np
 
 vertexes = np.loadtxt("ExampleInput.txt", dtype=np.float32)
-print(vertexes)
 vertexes_permutated = np.random.permutation(vertexes)
-print(vertexes_permutated)
 vertexes_permutated_index = [np.where(vertexes==i)[0][0] for i in vertexes_permutated]
-print(vertexes_permutated_index)
-vertexes_permutated_neighbours = [[vertexes[i-1],vertexes[(i+1)%7]] for i in vertexes_permutated_index]
-print(vertexes_permutated_neighbours)
+
+# deleted permunated vertex one by one, and record their neighbours
+neighbours_with_deletion = []
+vertexes_index = np.arange(len(vertexes))
+for i in vertexes_permutated_index:
+  i_index = np.where(vertexes_index==i)[0]
+  neighbours_ccw = vertexes_index[np.asscalar((i_index+1)%len(vertexes_index))]
+  neighbours_cw = vertexes_index[np.asscalar(i_index-1)]
+  neighbours_with_deletion.append([vertexes[neighbours_cw],vertexes[neighbours_ccw]])
+  vertexes_index = np.delete(vertexes_index,i_index)
+  print("current vertex",i, ",index in current convex hull is", i_index)
+  print("vertex",i,"remaining neighbours are ",neighbours_cw,neighbours_ccw)
+  print("deleted vertex",i,"from convex hull")
+  print("Remaining CH vertexes are:",vertexes_index)
+  if len(vertexes_index) <=2:
+    break
+print(neighbours_with_deletion)
