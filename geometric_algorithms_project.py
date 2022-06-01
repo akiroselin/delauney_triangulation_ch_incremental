cc0aa22442a637239efaa038fd276a1812d72087
Elie Youssef <elayus3@gmail.com>
2022-06-01 20:24:57 +0200

    Updating implementation

--- geometric_algorithms_project.py
+++ geometric_algorithms_project.py
@@ -14,11 +14,12 @@ class Point_With_Neighbor:
     prev_neighbor_index: int
     next_neighbor_index: int
 
-testpoint = Point(1,2)
+# testpoint = Point(1,2)
 
-p = Point_With_Neighbor(testpoint, 2, 3)
-
-print(p)  # Point(x=1.5, y=2.5, z=0.0)
+# p = Point_With_Neighbor(testpoint, 2, 3)
+# print(testpoint)
+# print(testpoint.x)
+# print(p)  # Point(x=1.5, y=2.5, z=0.0)
 
 vertices = np.loadtxt("ExampleInput.txt", dtype=np.float32)
 permuted_indices = np.random.permutation(np.arange(len(vertices)))
@@ -36,4 +37,5 @@ permuted_indices = np.random.permutation(np.arange(len(vertices)))
 # for vertex in vertices_permuted
 
 
-for index in permuted_indices:
+# for index in permuted_indices:
+
